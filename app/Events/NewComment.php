98169e8a54f28651aca716c9d63775339aa6c933
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-12-05 23:16:46 +0500

    adds queues to the NewComment event and it works

--- app/Events/NewComment.php
+++ app/Events/NewComment.php
@@ -12,7 +12,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class NewComment implements ShouldBroadcastNow
+class NewComment implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
