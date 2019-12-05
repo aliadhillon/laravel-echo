42bc87e03aafa67f650164faa67d77d5712ceb6a
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-12-06 00:02:38 +0500

    adds some more options in the Post channel

--- routes/channels.php
+++ routes/channels.php
@@ -18,5 +18,17 @@ Broadcast::channel('App.User.{id}', function ($user, $id) {
 });
 
 Broadcast::channel('post.{id}', function ($user, $id) {
-    return (int) $user->id === (int) Post::find($id)->user_id;
+    /**
+     * If you want all authenticated users to be able to subscribe to
+     * this channel then just return true becase only authenticated user
+     * can acess private channels no further authorization is required
+     */
+
+    return true;
+
+    /**
+     * But if you want only the user who posted this post to see be able
+     * to subscribe to this channel then you can do the follwing.
+     */
+    // return (int) $user->id === (int) Post::find($id)->user_id;
 });
